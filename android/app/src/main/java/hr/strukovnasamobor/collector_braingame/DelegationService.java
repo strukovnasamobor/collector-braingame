5835140bf1ae132da941108772c2b0655222ecbd
ZDamijan <zlatko.damijanic@gmail.com>
2026-05-21 08:57:09 +0200

    Refactor code structure for improved readability and maintainability

---
+++ android/app/src/main/java/hr/strukovnasamobor/collector_braingame/DelegationService.java
@@ -0,0 +1,14 @@
+package hr.strukovnasamobor.collector_braingame;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
